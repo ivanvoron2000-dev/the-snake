1f8d76717bfe3d0127d1e41ed772ab744a06aa01
Ivan Voronezhev <ivan.voronezhev@gmail.com>
2026-08-28 23:24:12 +0300

    сдача проекта изгиб питона

--- the_snake.py
+++ the_snake.py
@@ -1,4 +1,4 @@
-from random import choice, randint
+from random import randint
 
 import pygame
 
@@ -27,7 +27,10 @@ APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
 # Скорость движения змейки:
-SPEED = 20
+SPEED = 10
+
+# Ввводим размер квадратика:
+CELL_SIZE = 20
 
 # Настройка игрового окна:
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -39,68 +42,170 @@ pygame.display.set_caption('Змейка')
 clock = pygame.time.Clock()
 
 
-# Тут опишите все классы игры.
-...
+class GameObject:
+    """Общий класс для яблока и змейки"""
+
+    def __init__(self, body_color=None):
+        # Общие координаты
+        center_x = SCREEN_WIDTH // 2
+        center_y = SCREEN_HEIGHT // 2
+        # Сохраняем результат
+        self.position = (center_x, center_y)
+        # Общий цвет
+        self.body_color = body_color
+        # Активен ли объект
+        self.is_active = True
+
+    def draw(self, screen):
+        """Пустой метод для последующего объявления в дочерних классах"""
+        pass
+
+
+# Класс яблока:
+class Apple(GameObject):
+    """Класс яблока"""
+
+    def __init__(self, body_color=APPLE_COLOR):
+        super().__init__(body_color)
+
+    def randomize_position(self, snake_body=None):
+        """Генерация случайной позиции."""
+        while True:
+            # Генерируем случайную клетку (потом переведем в пиксели)
+            x_cell = randint(0, GRID_WIDTH - 1)
+            y_cell = randint(0, GRID_HEIGHT - 1)
+
+            new_pos = (x_cell * CELL_SIZE, y_cell * CELL_SIZE)
+
+            # ПРОВЕРКА: Если змейки нет или позиция свободна
+            if snake_body is None or new_pos not in snake_body:
+                self.position = new_pos
+                return
+
+    def draw(self, screen):
+        """Отрисовка яблока"""
+        rect = pygame.Rect(self.position, (CELL_SIZE, CELL_SIZE))
+        pygame.draw.rect(screen, self.body_color, rect)
+        pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+
+class Snake(GameObject):
+    """Класс змеи"""
+
+    def __init__(self, body_color=SNAKE_COLOR):
+        super().__init__(body_color)
+        self.length = 1
+        self.positions = [self.position]
+        self.direction = RIGHT
+        self.next_direction = None
+        self.last = None
+
+    def get_head_position(self):
+        """Определяем позицию головы змеи"""
+        if not self.positions:
+            return None
+        return self.positions[0]
+
+    def move(self):
+        """Логика движения змейки"""
+        current_head = self.get_head_position()
+        new_head_x = current_head[0] + self.direction[0] * CELL_SIZE
+        new_head_y = current_head[1] + self.direction[1] * CELL_SIZE
+
+        final_x = (new_head_x // CELL_SIZE) % GRID_WIDTH * CELL_SIZE
+        final_y = (new_head_y // CELL_SIZE) % GRID_HEIGHT * CELL_SIZE
+        final_head = (final_x, final_y)
+
+        self.positions.insert(0, final_head)
+
+        if len(self.positions) > self.length:
+            self.last = self.positions[-1]
+            self.positions.pop()
+        else:
+            self.last = None
+
+    def reset(self):
+        """Метод для возвращения змейки в изначальное состояние"""
+        center_x = SCREEN_WIDTH // 2
+        center_y = SCREEN_HEIGHT // 2
+        start_pos = (center_x, center_y)
+
+        self.length = 1
+        self.positions = [start_pos]
+
+        self.next_direction = None
+        self.last = None
+
+    def update_direction(self):
+        """Обновляет направление движения змеи, если игрок нажал клавишу"""
+        if self.next_direction is not None:
+
+            is_reverse = (
+                self.next_direction[0] == -self.direction[0] and
+                self.next_direction[1] == -self.direction[1]
+            )
+
+            if not is_reverse:
+                self.direction = self.next_direction
+
+            self.next_direction = None
+
+    def draw(self, screen):
+        """Метод для отрисовки змейки"""
+        for position in self.positions:
+            rect = pygame.Rect(position, (CELL_SIZE, CELL_SIZE))
+
+            pygame.draw.rect(screen, self.body_color, rect)
+
+            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+
+def handle_keys(game_object):
+    """Логика управления игроком"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and game_object.direction != DOWN:
+                game_object.next_direction = UP
+            elif event.key == pygame.K_DOWN and game_object.direction != UP:
+                game_object.next_direction = DOWN
+            elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+                game_object.next_direction = LEFT
+            elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+                game_object.next_direction = RIGHT
 
 
 def main():
-    # Инициализация PyGame:
+    """Игровой процесс"""
     pygame.init()
     # Тут нужно создать экземпляры классов.
-    ...
+    snake = Snake()
+    apple = Apple()
+    apple.randomize_position(snake.positions)
 
-    # while True:
-    #     clock.tick(SPEED)
-
-        # Тут опишите основную логику игры.
-        # ...
+    while True:
+        clock.tick(SPEED)
+        handle_keys(snake)
+        snake.update_direction()
+        # Двигаем змею
+        snake.move()
+        # Проверяем, не врезалась ли змея в себя
+        head = snake.get_head_position()
+        if head in snake.positions[1:]:
+            print("Игра окончена! Врезался в себя.")
+            snake.reset()
+            continue
+        # Рост змейки на деление, если съела яблоко
+        if head == apple.position:
+            snake.length += 1
+            apple.randomize_position(snake.positions)
+        screen.fill(BOARD_BACKGROUND_COLOR)
+        snake.draw(screen)
+        apple.draw(screen)
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
     main()
-
-
-# Метод draw класса Apple
-# def draw(self):
-#     rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-#     pygame.draw.rect(screen, self.body_color, rect)
-#     pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-
-# # Метод draw класса Snake
-# def draw(self):
-#     for position in self.positions[:-1]:
-#         rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
-#         pygame.draw.rect(screen, self.body_color, rect)
-#         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
-
-#     # Отрисовка головы змейки
-#     head_rect = pygame.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
-#     pygame.draw.rect(screen, self.body_color, head_rect)
-#     pygame.draw.rect(screen, BORDER_COLOR, head_rect, 1)
-
-#     # Затирание последнего сегмента
-#     if self.last:
-#         last_rect = pygame.Rect(self.last, (GRID_SIZE, GRID_SIZE))
-#         pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
-
-# Функция обработки действий пользователя
-# def handle_keys(game_object):
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             raise SystemExit
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_UP and game_object.direction != DOWN:
-#                 game_object.next_direction = UP
-#             elif event.key == pygame.K_DOWN and game_object.direction != UP:
-#                 game_object.next_direction = DOWN
-#             elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
-#                 game_object.next_direction = LEFT
-#             elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
-#                 game_object.next_direction = RIGHT
-
-# Метод обновления направления после нажатия на кнопку
-# def update_direction(self):
-#     if self.next_direction:
-#         self.direction = self.next_direction
-#         self.next_direction = None
